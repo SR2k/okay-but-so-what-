3391ce61f86694e23ec22abc802f1861a7b86179
Siyuan Cao <seeran@outlook.com>
2019-08-30 15:51:47 +0800

    feat: update bezier demo

--- src/playground/bezier/bezier.ts
+++ src/playground/bezier/bezier.ts
@@ -8,8 +8,8 @@ declare function requestAnimationFrame(fn: Function): void
 declare interface VueOptions {
   el: string
   data: any
-  computed: { [key: string]: Function }
-  methods: { [key: string]: Function }
+  computed?: { [key: string]: Function }
+  methods?: { [key: string]: Function }
 }
 declare class Vue {
   constructor(options: VueOptions)
@@ -51,7 +51,7 @@ const getBezier = (precision: number, ...points: Point[]) => {
 const limit = (x: number) => {
   if (x >= 1) return 1
   if (x <= 0) return 0
-  return x
+  return parseFloat(x.toFixed(3))
 }
 
 const getMousePos = ({ clientX, clientY }: MouseEvent, point: Point) => {
@@ -64,8 +64,51 @@ const getTouchPos = ({ touches }: TouchEvent, point: Point) => {
   point.y = (touches[0].clientY)
 }
 
+function startMove(self: any, flag: string, evt: MouseEvent & TouchEvent) {
+  const touch = (evt.type === 'touchstart')
+  if (!touch && evt.button !== 0) return
+  const events = touch ? {
+    move: 'touchmove',
+    stop: 'touchend',
+  } : {
+    move: 'mousemove',
+    stop: 'mouseup',
+  }
+  const elem = (<any>evt.currentTarget).closest('svg')
+  const point = elem.createSVGPoint()
+  const transform = elem.getScreenCTM().inverse()
+  const circlePos = typeof flag === 'string' ? (flag === 'a' ? self.a : self.b) : self.dots[flag]
+  const getPos = touch ? getTouchPos : getMousePos
+
+  let moving = true
+  let newPt
+
+  const updateFn = () => {
+    if (moving) requestAnimationFrame(updateFn)
+
+    newPt = point.matrixTransform(transform)
+    const x = (newPt.x - 5) / 100
+    const y = 1 - (newPt.y - 5) / 100
+
+    circlePos.x = limit(x)
+    circlePos.y = limit(y)
+  }
+  const moveFn = (evt: MouseEvent & TouchEvent) => getPos(evt, point)
+  const stopFn = () => {
+    moving = false
+    elem.removeEventListener(events.move, moveFn)
+    elem.removeEventListener(events.stop, stopFn)
+  }
+
+  requestAnimationFrame(updateFn)
+  moveFn(evt)
+
+  elem.addEventListener(events.move, moveFn)
+  elem.addEventListener(events.stop, stopFn)
+}
+
 new Vue({
-  el: '#bezier',
+  el: '#cubicBezier',
   data: {
     a: {x: 0, y: 1},
     b: {x: 1, y: 0},
@@ -93,46 +136,45 @@ new Vue({
   },
   methods: {
     startMove(flag: string, evt: MouseEvent & TouchEvent) {
-      const touch = (evt.type === 'touchstart')
-      if (!touch && evt.button !== 0) return
-      const events = touch ? {
-        move: 'touchmove',
-        stop: 'touchend',
-      } : {
-        move: 'mousemove',
-        stop: 'mouseup',
-      }
-      const elem = (<any>evt.currentTarget).closest('svg')
-      const point = elem.createSVGPoint()
-      const transform = elem.getScreenCTM().inverse()
-      const circlePos = flag === 'a' ? this.a : this.b
-      const getPos = touch ? getTouchPos : getMousePos
+      startMove(this, flag, evt)
+    },
+  }
+})
 
-      let moving = true
-      let newPt
-
-      const updateFn = () => {
-        if (moving) requestAnimationFrame(updateFn)
-
-        newPt = point.matrixTransform(transform)
-        const x = (newPt.x - 5) / 100
-        const y = 1 - (newPt.y - 5) / 100
-
-        circlePos.x = limit(x)
-        circlePos.y = limit(y)
-      }
-      const moveFn = (evt: MouseEvent & TouchEvent) => getPos(evt, point)
-      const stopFn = () => {
-        moving = false
-        elem.removeEventListener(events.move, moveFn)
-        elem.removeEventListener(events.stop, stopFn)
-      }
-
-      requestAnimationFrame(updateFn)
-      moveFn(evt)
-
-      elem.addEventListener(events.move, moveFn)
-      elem.addEventListener(events.stop, stopFn)
+new Vue({
+  el: '#bezier',
+  data: {
+    precision: 100,
+    dots: [
+      {
+        x: 0.199,
+        y: 0.702,
+      }, {
+        x: 0.131,
+        y: 0.455,
+      }, {
+        x: 0.666,
+        y: 0.777,
+      },
+    ]
+  },
+  computed: {
+    points() {
+      return getBezier(this.precision, ...this.dots)
     },
   },
+  methods: {
+    addDot() {
+      this.dots.push({
+        x: 0.5,
+        y: 0.5
+      })
+    },
+    removeDot(index: number) {
+      this.dots.splice(index, 1)
+    },
+    startMove(flag: string, evt: MouseEvent & TouchEvent) {
+      startMove(this, flag, evt)
+    },
+  }
 })
